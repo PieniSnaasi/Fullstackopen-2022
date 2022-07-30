10ffe7b9f5e13b3a3cf7c61a94ce064cd2bf2105
Aapo <105632067+PieniSnaasi@users.noreply.github.com>
2022-07-30 16:19:42 +0300

    upload 1.2 and 1.3

---
+++ Osa1/kurssitiedot/1.2/src/App.js
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.teht}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} teht={props.teht1}/>
+      <Part part={props.part2} teht={props.teht2}/>
+      <Part part={props.part3} teht={props.teht3}/>
+      
+
+      
+    </div> 
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.lukumaara}</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+ 
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const part2= "Using props to pass data"
+  const part3= "State of a component"
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  const yhteisluku = exercises1 + exercises2 + exercises3
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} teht1={exercises1} />
+      <Content part2={part2} teht2={exercises2} />
+      <Content part3={part3} teht3={exercises3} />
+      <Total lukumaara={yhteisluku} />
+     
+    </div>
+  )
+}
+
+export default App
